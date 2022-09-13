a4b174ab2187470f77b6e7ee04d169b6b313df90
Kumo <heinhtet25944.com>
2022-09-13 16:35:25 +0630

    update carousel

--- WelcomeCarousel.js
+++ WelcomeCarousel.js
@@ -1,5 +1,14 @@
 import * as React from "react";
-import { Text, View, SafeAreaView, Button } from "react-native";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  Button,
+  StyleSheet,
+  Image,
+  ImageBackground,
+} from "react-native";
+import IMG_1 from "./assets/images/welcome_1.jpg";
 
 import Carousel from "react-native-snap-carousel";
 
@@ -12,22 +21,17 @@ export default class WelcomeCarousel extends React.Component {
         {
           title: "Item 1",
           text: "Text 1",
+          imageURL: require("./assets/images/welcome_1.jpg"),
         },
         {
           title: "Item 2",
           text: "Text 2",
+          imageURL: require("./assets/images/welcome_2.jpg"),
         },
         {
-          title: "Item 3",
+          title: "ပြည်ထောင်စုဖောင့် ဒေါင်းလုဒ်",
           text: "Text 3",
-        },
-        {
-          title: "Item 4",
-          text: "Text 4",
-        },
-        {
-          title: "Item 5",
-          text: "Text 5",
+          imageURL: require("./assets/images/welcome_3.jpg"),
         },
       ],
     };
@@ -35,18 +39,34 @@ export default class WelcomeCarousel extends React.Component {
 
   _renderItem({ item, index }) {
     return (
-      <View
-        style={{
-          backgroundColor: "floralwhite",
-          borderRadius: 5,
-          height: 450,
-          padding: 50,
-          
-        }}
+      <ImageBackground
+        style={styles.tinyLogo}
+        source={item.imageURL}
+        resizeMode="cover"
+        borderRadius={8}
       >
-        <Text style={{ fontSize: 30 }}>{item.title}</Text>
-        <Text>{item.text}</Text>
-      </View>
+        <View
+          style={{
+            backgroundColor: "transparent",
+            borderRadius: 5,
+
+            // height: 450,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 30,
+              color: "#ffffff",
+              textShadowColor: "rgba(0, 0, 0, 0.8)",
+              textShadowOffset: { width: 0, height: 0 },
+              textShadowRadius: 4,
+            }}
+          >
+            {item.title}
+          </Text>
+          <Text>{item.text}</Text>
+        </View>
+      </ImageBackground>
     );
   }
 
@@ -54,7 +74,12 @@ export default class WelcomeCarousel extends React.Component {
     return (
       <SafeAreaView style={{ flex: 1, paddingTop: 20 }}>
         <View
-          style={{ flex: 1, flexDirection: "row", justifyContent: "center",padding:10 }}
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            justifyContent: "center",
+            padding: 10,
+          }}
         >
           <Carousel
             sliderHeight={10}
@@ -73,3 +98,12 @@ export default class WelcomeCarousel extends React.Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  tinyLogo: {
+    backgroundColor: "floralwhite",
+    borderRadius: 5,
+    height: 450,
+    padding: 50,
+  },
+});
